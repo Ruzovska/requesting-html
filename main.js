083e54fd0c22533860823ce497e41f73db9c52e2
Ruzovska <nastybonda@gmail.com>
2018-02-14 20:23:27 +0700

    using settings

--- main.js
+++ main.js
@@ -1,20 +1,4 @@
-var cat = require("fs").readFileSync('meow.txt','utf8');
-console.log(cat);
-
-// require('request')({uri: 'https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)',}, function (error, response, body) {console.log(body);
-// });
-//
-var a = {uri: 'https://en.wikipedia.org/wiki/List_of_countries_by_population_(United_Nations)',};
-var globalstring;
-var b = function (error, response, body) {
-	console.log('body = ' + body);
-	console.log('body is ' + typeof(body) + ' in the function');
-	globalstring = body;
-	console.log('globalstring inside = ' + globalstring);
-	console.log('globalstring is ' + typeof(globalstring) + ' in the function');
-};
-require('request')(a, b);
-console.log('body is globally ' + typeof(body));
-console.log('globalstring is globally ' + typeof(globalstring));
-console.log('globalstring outside = ' + globalstring);
-//
+var settings = require('./settings.json');
+var urii = settings['uri'];
+require('request')({uri: urii,}, function (error, response, body) {console.log(body);
+});
